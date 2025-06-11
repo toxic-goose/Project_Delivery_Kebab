0ab4b5984125806c9d6473c9f638e0be0060221d
KirillPanov163 <bendif163@gmail.com>
2025-06-11 20:44:24 +0300

    serverstarted

--- server/src/routes/index.router.js
+++ server/src/routes/index.router.js
@@ -1,10 +1,11 @@
 const indexRouter = require('express').Router()
-
-const mainRouter = require('./main.router')
+const orderRouter = require('./order.router')
+// const mainRouter = require('./main.router')
 const authRouter = require('./auth.router')
 
-indexRouter.use('/', mainRouter)
+// indexRouter.use('/', mainRouter)
 indexRouter.use('/auth', authRouter)
+indexRouter.use('/order', orderRouter)
 
 indexRouter.use((req, res) => {
   res.status(404).send('Страница не найдена');
