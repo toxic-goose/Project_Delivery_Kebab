d6aef03fb91d72b3cb8d7415da5310f70101bfb0
7Eighty <igrovoi.ac@yandex.ru>
2025-06-11 18:24:38 +0300

    addFile/userApi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,73 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UserApi from './entities/UserApi'
+import { setAccessToken } from './shared/lib/axiosInstance' 
+import { UserValidator } from './entities/users/User.validator'
+
+const defaultValue = {
+  login: "",
+  mail: "",
+  password: "",
+  id: 0,
+};
 
 function App() {
   const [count, setCount] = useState(0)
+  const [ user, setUser ] = useState({})
+    const [orders, setOrders] = useState([]);
+  const [inputs, setInputs] = useState(defaultValue);
+  const [validateError, setValidateError] = useState('')
+
+  useEffect(() => {
+    console.log('Зашли в useEffect')
+    const getUser = async () => {
+      try {
+        const data = await UserApi.refresh()
+        console.log("refresh data:", data)
+        if (data.statusCode === 200 && data.data.accessToken) {
+          // ! ! ! ! ! !
+          setUser((pre) => ({...pre, ...data.data.user}))
+          setAccessToken(data.data.accessToken)
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getUser()
+  }, [])
+
+  async function submitHandler(e) {
+    e.preventDefault();
+    try {
+      const { isValid, error } = UserValidator.validate(inputs)
+      
+      if (isValid) {
+        const data = await UserApi.register(inputs)
+        if (data.statusCode === 200 && data.data.accessToken) {
+          setOrders((orders) => [...orders, data.data.user])
+          setInputs(defaultValue);
+          setValidateError('')
+        } else {
+          console.log(error)
+        }
+      } else {
+        setValidateError(error)
+        console.log('Ошибка из валидатора', error)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  function inputsHandler(e) {
+    setInputs((inputs) => ({ ...inputs, [e.target.name]: e.target.value }));
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
