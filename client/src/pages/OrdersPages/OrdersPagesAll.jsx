18c2a0a1d9f3cbf927ed368124c8ae7c056d1b8d
KirillPanov163 <bendif163@gmail.com>
2025-06-12 14:08:25 +0300

    pagesall

--- client/src/pages/OrdersPages/OrdersPagesAll.jsx
+++ client/src/pages/OrdersPages/OrdersPagesAll.jsx
@@ -18,29 +18,18 @@ export default function OrdersPagesAll() {
 
     console.log(orders);
     
-    // const SubmitHandler = async () => { 
-    //     const data = await OrdersApi.getAll()
-    //     console.log(data);
-        
-    // return data    
-    // }
     
-
-
-// const data = await OrdersApi.getAll
 return (
     <div>1234
-    
     {orders.map((el) =>
         <div key={el.id}>
         <hr />
         <h3>{el.order_name}</h3>
-        <h4>{el.description}</h4>
-        <h4>{el.description}</h4>
-        <h4>{el.price}</h4>
+            <h4>{el.img_path}</h4> 
+        <button>Подробнее</button>    
         <hr />
         </div> 
-        // <UserCard orders={orders}  el={el} key={el.id} />
+        
     )}
     
     </div>
