b24ef91c8564b72b5950cd35f69e82dbf58365b5
toxic-goose <9110229459@mail.ru>
2025-06-12 22:03:56 +0300

    feat_Basket.Service

---
+++ server/src/services/Basket.service.js
@@ -0,0 +1,32 @@
+const { Basket } = require ('../db/models')
+
+class BasketService {
+
+    //* Получение всех получение заказов
+
+    static async getAllBasket() {
+        const baskets = await Basket.findAll()
+        const result = baskets.map((el) => el.get({ plain: true }))
+    return result
+    }
+
+    //* Получение одного заказа
+
+    static async getOneUser(id) {
+        const basket = await Basket.findByPk(id);
+        const result = basket.get({ plain: true })
+    return result
+    }
+
+    
+
+    //* удаление заказа
+
+    static async deleteBasket(id){
+        const basket = await Basket.findByPk(id)
+        basket.destroy()
+        return id
+    }
+}
+
+module.exports = BasketService
