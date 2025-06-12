c5ed934a0710b7ffa8b8751b0f9c0cef05634a9b
7Eighty <igrovoi.ac@yandex.ru>
2025-06-13 01:04:39 +0300

    feature/multer

--- client/src/pages/Main/MainPage.jsx
+++ client/src/pages/Main/MainPage.jsx
@@ -1,25 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import "./MainPage.css";
 
 export default function MainPage() {
   const navigate = useNavigate();
-  // Пример массива с URL изображений с сервера
   const images = [
     "http://localhost:3000/uploads/Devchata.jpg",
     "http://localhost:3000/uploads/test.png",
-
+    "http://localhost:3000/uploads/Headliners_20252615527.jpg",
+    "http://localhost:3000/uploads/Ricardo.jpg",
+    "http://localhost:3000/uploads/shreckOnil.jpg",
+    "http://localhost:3000/uploads/IMG_20200619_112457.jpg",
+    "http://localhost:3000/uploads/IMG_20200508_201150.jpg",
+    "http://localhost:3000/uploads/IMG_20200208_091506.jpg",
+    "http://localhost:3000/uploads/IMG_20191109_112514.jpg",
   ];
+
+  const itemsToShow = 3;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const maxIndex = images.length - itemsToShow;
+  const nextImage = () => {
+    setCurrentIndex((prev) => (prev < maxIndex ? prev + 1 : prev));
+  };
+  const prevImage = () => {
+    setCurrentIndex((prev) => (prev > 0 ? prev - 1 : 0));
+  };
+  
   return (
-    <div className="banner">
-      {images.map((url, index) => (
-        <div key={index} className="banner-image-wrapper" onClick={() => {
-          // При клике, например, можно перейти на страницу с подробностями
-          navigate(`/image/${index}`);
-        }}>
-          <img src={url} alt={`Uploaded #${index + 1}`} className="banner-image" />
-        </div>
-      ))}
+<div className="banner">
+      <button className="banner-arrow left" onClick={prevImage} aria-label="Previous Image" disabled={currentIndex === 0}>
+        ❮
+      </button>
+      <div
+        className="banner-slider"
+        style={{
+          transform: `translateX(-${(currentIndex * 100) / itemsToShow}%)`,
+        }}
+      >
+        {images.map((url, index) => (
+          <div key={index} className="banner-image-wrapper">
+            <img
+              src={url}
+              alt={`Uploaded #${index + 1}`}
+              className="banner-image"
+              onClick={() => navigate(`/image/${index}`)}
+            />
+          </div>
+          ))}
+      </div>
+      <button
+        className="banner-arrow right"
+        onClick={nextImage}
+        aria-label="Next Image"
+        disabled={currentIndex === maxIndex}
+      >
+        ❯
+      </button>
     </div>
   );
 }
